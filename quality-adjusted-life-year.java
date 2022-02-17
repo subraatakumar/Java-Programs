21b756a2bd9c70ee3a9dff0920ba2c9999f96c73
Subrat Kumar Das <72245773+subratsir@users.noreply.github.com>
2022-02-17 07:20:36 +0530

    Update quality-adjusted-life-year.java

--- quality-adjusted-life-year.java
+++ quality-adjusted-life-year.java
@@ -1,1 +1,20 @@
 
+import java.util.Scanner;
+
+public class qualityAdjustedLifeYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		float result = 0;
+		for(int i=0; i<N; i++) {
+			float q = sc.nextFloat();
+			float y = sc.nextFloat();
+			result = result + (q*y);
+		}
+		System.out.format("%.3f",result);
+		
+	}
+
+}
