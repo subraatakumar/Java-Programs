0160b939113a5b56f69dad86d61f0927a8b81375
Subrat Kumar Das <72245773+subratsir@users.noreply.github.com>
2022-02-21 06:47:36 +0530

    Create digitalswap.java

---
+++ digitalswap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class digitSwap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int i = N/10;
+		int j = N%10;
+		System.out.println(j + "" + i);
+		
+
+	}
+
+}
